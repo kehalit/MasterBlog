028870351208c2ee828c66edaf8e64f425badb13
kehalit <kahineb.kn@gmail.com>
2025-03-10 23:10:40 +0100

    feat: add routes to display and create blog posts

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from xml.etree.ElementTree import indent
-
-from flask import Flask, render_template, request
 import json
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import redirect
 
 
-blog_posts = [
-    {'id': 1, 'author': 'John Doe', 'title': 'First Post', 'content': 'This is my first post.'},
-    {'id': 2, 'author': 'Jane Doe', 'title': 'Second Post', 'content': 'This is another post.'},
-    # More blog posts can go here...
-]
+def load_posts():
+    try:
+        with open('DB/blog_posts.json', 'r') as file:
+            data = json.load(file)
+            if not isinstance(data, list):
+                return []
+            return data
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
-with open ('DB/blog_posts.json', 'w') as file:
-    json.dump(blog_posts, file, indent=4)
-
+def save_posts(posts):
+    with open('DB/blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
 
 app = Flask(__name__)
 
@@ -20,9 +23,31 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    with open ('DB/blog_posts.json', 'r') as file:
-        blog_posts = json.load(file)
-    return render_template('index.html', posts=blog_posts)
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        posts = load_posts()
+
+        new_post = {
+                'id': posts[-1]['id'] + 1 if posts else 1,
+                    'author': author,
+                    'title': title,
+                    'content': content
+        }
+
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
 
 
 if __name__ == '__main__':
